a5d32587966e337bb473eba5e605136db652c9b7
fronz <z233@msn.com>
2022-05-27 12:13:10 +0800

    chores: update blog config

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'Fronz',
+  title: 'Fronz 的个人博客',
   author: 'Fronz',
   email: 'z233@msn.com',
   link: 'https://z233.me',
-  description: 'This gonna be an awesome website.',
+  description: 'Fronz 的个人博客。',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
@@ -19,7 +19,7 @@ const BLOG = {
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: '',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
+    keywords: ['Blog', 'Website', 'Notion', 'SuperMemo'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
